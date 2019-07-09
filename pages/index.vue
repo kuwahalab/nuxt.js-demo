b57e8ded84fce66fb5c9b2972ad76e6a11b5fadc
kuwahalab <kuwahara.shohei@slogan.jp>
2019-07-09 19:53:38 +0900

    jsonを読み込み記事一覧を表示した。

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        demo
+        Demo
       </h1>
-      <h2 class="subtitle">
-        My tremendous Nuxt.js project
-      </h2>
+      <h2 class="subtitle" />
+      <h3>新着記事一覧</h3>
+      <card-list
+        class="contents"
+        :posts="posts"
+      />
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
+          href="https://google.com"
           target="_blank"
           class="button--green"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
+          Google
         </a>
       </div>
     </div>
@@ -29,19 +24,27 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import CardList from '~/components/CardList.vue'
+
+import posts from '~/contents/posts/json/index.json'
 
 export default {
   components: {
-    Logo
+    CardList
+  },
+  data() {
+    return {
+      posts: posts
+    }
   }
+
 }
 </script>
 
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  min-height: 50vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -53,14 +56,14 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 50px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 21px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
