ab749e876f9fda237f0e26526102a2288ae674a1
kuwahalab <kuwahara.shohei@slogan.jp>
2019-07-09 19:49:16 +0900

    記事一覧コンポーネントの作成をした。

---
+++ components/CardList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-list">
+    <card
+      v-for="post in posts"
+      :key="post.id"
+      class="card"
+      :title="post.title"
+      :category="post.category"
+      :tags="post.tags"
+      :created-at="post.createdAt"
+      :published-date="post.publishedDate"
+      :slug="post.slug"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  components: { Card },
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
